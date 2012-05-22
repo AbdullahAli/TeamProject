da6fd634933d04e58b9305595551dd49c9e894c7
Daniel Fernandez <Daniel.Fernandez@migcan.com>
2012-05-22 16:53:48 +0100

    Added functionality to return HashMap of UserTypes

---
+++ InfoPharma/src/infopharma/data/AccountDBAccess.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infopharma.data;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+/**
+ *
+ * @author dfernandez
+ */
+public class AccountDBAccess extends DBAccess{
+    
+    public HashMap<Integer, String> getUserTypes(){
+        HashMap<Integer, String> userTypes = new HashMap<Integer, String>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String sql = "SELECT * FROM UserRoles";
+        try{
+            connection = makeConnection();
+            statement = (Statement) connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            while(resultSet.next()){
+                int roleID = resultSet.getInt("roleID");
+                String roleType = resultSet.getString("roleType");
+                userTypes.put(roleID, roleType);
+            }
+        }catch(SQLException ex){
+            
+        }
+        return userTypes;
+    }
+    
+}
