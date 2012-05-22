b2960653d39703d6ee164a9932bb13323c7cfd21
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2012-05-22 12:02:43 +0100

    added basic main frame

---
+++ InfoPharma/src/infopharma/InfoPharma.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infopharma;
+
+import infopharma.acc.InfoPharmaFrame;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class InfoPharma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new InfoPharmaFrame();
+        
+    }
+}
