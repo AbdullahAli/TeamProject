afa0c0504b1af4fbf6e09a849c3c43ecf5091e82
Daniel Fernandez <Daniel.Fernandez@migcan.com>
2012-05-22 18:51:38 +0100

    added finally to try/catch blocks

--- InfoPharma/src/infopharma/data/AccountDBAccess.java
+++ InfoPharma/src/infopharma/data/AccountDBAccess.java
@@ -34,6 +34,8 @@ public class AccountDBAccess extends DBAccess{
             }
         }catch(SQLException ex){
             
+        }finally{
+            closeConnection(connection);
         }
         return userTypes;
     }
@@ -54,6 +56,8 @@ public class AccountDBAccess extends DBAccess{
             }
         }catch(SQLException ex){
             
+        }finally{
+            closeConnection(connection);
         }
         return discountPlans;
     }
